4593476e7328e722bcb4b17401c82421bcb6bd6a
Ajay Sahani <sajay3159@gmail.com>
2024-02-21 00:00:50 +0530

    bottom footer responsive done

--- src/component/BottomFooter.jsx
+++ src/component/BottomFooter.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 
 const BottomFooter = () => {
     return (
-        <Container fluid className='d-flex justify-content-around align-items-center bottomfooter py-2'>
-            <div className='text-white'>© 2022 Coral , Inc.</div>
-            <div><img src="/images/icons_payment 1.png" alt="card" /></div>
-            <div className='text-white'>
-                <span className='d-none d-md-inline-block' style={{ marginRight: '10px' }}>Scroll to top </span>
-                <img src="/images/Vector 9.png" alt="Up arrow" />
-            </div>
+        <Container fluid className='bottomfooter py-2'>
+            <Row className='justify-content-around align-items-center'>
+                <Col xs={12} md='auto' className='text-center text-md-start'>
+                    <div className='text-white'>© 2022 Coral , Inc.</div>
+                </Col>
+                <Col xs={12} md='auto' className='text-center'>
+                    <img src="/images/icons_payment 1.png" alt="card" />
+                </Col>
+                <Col xs={12} md='auto' className='text-center text-md-end'>
+                    <div className='text-white'>
+                        <span className='d-none d-md-inline-block' style={{ marginRight: '10px' }}>Scroll to top </span>
+                        <img src="/images/Vector 9.png" alt="Up arrow" />
+                    </div>
+                </Col>
+            </Row>
         </Container>
     );
 };
